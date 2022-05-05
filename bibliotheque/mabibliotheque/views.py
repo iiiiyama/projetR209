5ff174897106a5ff2ba554782196e225b510c8f3
iiiiyama <bptstantony@gmail.com>
2022-05-05 16:34:28 +0200

    projet

--- bibliotheque/mabibliotheque/views.py
+++ bibliotheque/mabibliotheque/views.py
@@ -1,17 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 
 # Create your views here.
 from .forms import FilmForm
 from . import models
 
-def index(request):
-    return render(request, 'mabibliotheque/index.html')
-
 def ajout(request):
     if request.method == "POST":
         form = FilmForm(request)
         if form.is_valid():
-            livre = form.save()  # sauvegarde dans la base
+            livre = form.save()
             return render(request, "mabibliotheque/affiche.html", {"livre": livre})
         else:
             return render(request, "mabibliotheque/ajout.html", {"form": form})
@@ -23,12 +20,33 @@ def traitement(request):
     lform = FilmForm(request.POST)
     if lform.is_valid():
         film = lform.save()
-        return render(request,"/bibliotheque/affiche.html",{"film" : film})
+        return HttpResponseRedirect("/bibliotheque/")
     else:
-        return render(request,"bibliotheque/ajout.html",{"form": lform})
+        return render(request,"bibliotheque/ajout.html", {"form": lform})
+
+def index(request):
+    liste = list(models.Film.objects.all())
+    return render(request,"mabibliotheque/index.html",{"liste": liste})
+
 def affiche(request, id):
     film = models.Film.objects.get(pk=id)
     return render(request, "bibliotheque/affiche.html", {"film": film})
+
 def formulaire(request):
     film = models.Film.objects.get(pk=id)
     return render(request, "bibliotheque/formulaire.html", {"film": film})
+
+def update(request, id):
+    film  =models.Film.objects.get(pk=id)
+    form = FilmForm(film.repertoire())
+    return render(request, "film/ajout.html",{"form":form, "id":id})
+
+def updatetraitement(request, id):
+    lform = FilmForm(request.POST)
+    if lform.is_valid():
+        film = lform.save(commit = False)
+        film.id = id
+        film.save()
+        return HttpResponseRedirect("/bibliotheque/")
+    else:
+        return render(request, "bibliotheque/ajout.html", {"form": lform, "id": id})
